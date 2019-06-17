5eb971ae0a883910284888c05219d171d5f841d4
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-16 22:52:16 -0700

    [feat] add route to home and component projects to display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/header';
+import ProjectsComponent from './components/projects';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Route path="/" exact component={App} />
+      <Route path="/" exact component={ProjectsComponent} />
+      <Footer />
     </div>
   );
 }

307e4c37c9cdef3cb65584202112b5d59bc0ed80
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-17 21:08:16 -0700

    [feat] create components linked in navigation bar

---
+++ src/components/about/contact-container.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContactContainer = () => <div>this is the contact form</div>;
+
+export default ContactContainer;
---
+++ src/components/blogs/blog-dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BlogDashboard = () => (
+  <div>
+    <p>this is the blog dashboard</p>
+  </div>
+);
+
+export default BlogDashboard;

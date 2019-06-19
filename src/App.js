e6cfd8ca34cd65dc353b9cf2bc8f1aa8d0cfeaf5
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-19 14:43:36 -0700

    [fix] bug of infinite loop due to routing issues

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
-        <Redirect from="/" to="/projects" />
-        <Route path="/projects" exact component={ProjectsContainer} />
+        <Redirect from="/projects" to="/" />
+        <Route path="/" exact component={ProjectsContainer} />
       </Switch>
       <Footer />
     </div>

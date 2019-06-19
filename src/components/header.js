0e549364edf1579b0e11421b750c316112e3e741
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-19 14:48:02 -0700

    [style] set mobile-first styles of navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,34 @@ import { Link, Route } from 'react-router-dom';
 import ProjectsContainer from './home/projects-container';
 import BlogDashboard from './blogs/blog-dashboard';
 import ContactContainer from './about/contact-container';
+import julie from '../styles/assets/images/julie_final.jpg';
 
 const Header = () => {
   return (
-    <div className="portfolio__navigation">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Projects</Link>
+    <div className="navigation__top--container">
+      <nav className="navigation__top">
+        <ul className="navigation__list">
+          <li className="navigation__list--home-links">
+            <Link to="/" className="navigation__link--home">
+              <img
+                src={`${julie}`}
+                className="navigation__image-link--home"
+                alt="Julie Hutchinson's thumbnail"
+              />
+              <span className="navigation__link-home--text">
+                Julie Hutchinson
+              </span>
+            </Link>
           </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
+          <li className="navigation__list--links">
+            <Link to="/blogs" className="navigation__link">
+              Blogs
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className="navigation__list--links">
+            <Link to="/about" className="navigation__link">
+              About
+            </Link>
           </li>
         </ul>
       </nav>

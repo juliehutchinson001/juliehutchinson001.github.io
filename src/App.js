5a847b3a571c77f218f634c296749c4dad0c6505
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-20 08:50:49 -0700

    [refactor] relocate routes to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/header';
 import ProjectsContainer from './components/home/projects-container';
+import BlogDashboard from './components/blogs/blog-dashboard';
+import ContactContainer from './components/about/contact-container';
 import Footer from './components/footer';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Redirect from="/projects" to="/" />
         <Route path="/" exact component={ProjectsContainer} />
+        <Route path="/blogs" component={BlogDashboard} />
+        <Route path="/about" component={ContactContainer} />
       </Switch>
       <Footer />
     </div>

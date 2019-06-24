0fbcd3c05f2aa416d8e90338b2d452184165fb79
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-23 19:10:49 -0700

    [test] finish testing to the about section of the portfolio

--- src/__test__/about.test.js
+++ src/__test__/about.test.js
@@ -3,4 +3,18 @@ import BioContainer from '../components/about/about-me';
 
 describe('BioContainer Component fully render', () => {
   const bioContainer = shallowRenderComponent(BioContainer);
+
+  it('fully renders its children', () => {
+    expect.assertions(4);
+    expect(bioContainer.children().length).toBe(2);
+    expect(
+      bioContainer.find('[data-test-bio-intro-container]').children().length
+    ).toBe(4);
+    expect(
+      bioContainer.find('[data-test-bio-secondary-info]').children().length
+    ).toBe(2);
+    expect(
+      bioContainer.find('[data-test-bio-list-of-skills]').children().length
+    ).toBe(7);
+  });
 });

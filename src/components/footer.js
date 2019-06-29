84c1fdf7271ebae569559609c13c986dfa8588d2
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-28 18:40:11 -0700

    [refactor] erase contact page and moved links to footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className="portfolio__footer" data-test-portfolio-footer-wrapper>
     <small className="footer__additional-info" data-test-footer-additional-info>
       Click on the image for project details
     </small>
-    <Link
+    <a
       className="footer__contact-link"
-      to="/contact"
       data-test-portfolio-contact-link
+      href="mailto:julie.hutchinson001@outlook.com"
     >
-      GET IN TOUCH
-    </Link>
+      EMAIL
+    </a>
+    <a
+      className="footer__contact-link"
+      data-test-portfolio-contact-link
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/juliehutchinson001"
+    >
+      GITHUB
+    </a>
+    <a
+      className="footer__contact-link"
+      data-test-portfolio-contact-link
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.linkedin.com/in/julie-hutchinson/"
+    >
+      LINKEDIN
+    </a>
   </footer>
 );
 

a0eff5f5ce0b68c4d2cab086b02fe185343907be
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-30 18:45:24 -0700

    [init] set up helper to abstract blog information

---
+++ src/components/helpers/blogs-info.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-param-reassign */
+
+let slugifiedName;
+
+const blogs = [
+  {
+    name: 'First Blog',
+    description:
+      'React, JavaScript, DOM manipulation, HTML5, React router, ES6-ES9, Redux, AJAX, jQuery',
+  },
+  {
+    name: 'Second Blog',
+    description:
+      'React, JavaScript, DOM manipulation, HTML5, React router, ES6-ES9, Redux, AJAX, jQuery',
+  },
+];
+
+export default blogs;

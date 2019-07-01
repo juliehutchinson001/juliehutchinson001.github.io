c66eea89328edcad64153f2b63c99798c4d2471d
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-06-30 18:27:17 -0700

    [refactor] abstract skills section to a component

--- src/components/about/about-me.js
+++ src/components/about/about-me.js
@@ -2,54 +2,25 @@ import React from 'react';
 import skills from '../helpers/skills-info';
 
 const Skills = () => {
+  const allSkills = skills.map(skillInfo => {
+    const { section, description } = skillInfo;
+    const bioClass = section.includes('Unix')
+      ? 'bio-skill__header terminal'
+      : 'bio-skill__header';
+    return (
+      <li className="bio-skill" data-test-bio-skill>
+        <h3 className={bioClass}>{section}</h3>
+        <p className="bio-skill__content">{description}</p>
+      </li>
+    );
+  });
   return (
     <section className="bio__secondary--info" data-test-bio-secondary-info>
       <h2 className="bio__subheader" data-test-bio-subheader>
         Skills
       </h2>
       <ul className="bio__list-of-skills" data-test-bio-list-of-skills>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">Frontend</h3>
-          <p className="bio-skill__content">
-            React, JavaScript, DOM, HTML5, React router, ES6-ES9, Redux, AJAX,
-            jQuery.
-          </p>
-        </li>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">CSS & Design</h3>
-          <p className="bio-skill__content">
-            CSS3, Sass, BEM, Semantic UI, CSS in JS (Aphrodite), Bootstrap.
-          </p>
-        </li>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">Testing</h3>
-          <p className="bio-skill__content">Jest, Enzyme, TDD.</p>
-        </li>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">CS</h3>
-          <p className="bio-skill__content">
-            OOP Fundamentals, Data Structures, Algorithms, Markov Chain.
-          </p>
-        </li>
-
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header terminal">Version Control / Unix</h3>
-          <p className="bio-skill__content">
-            Git/GitHub collaboration, Unix/Linux Command Line.
-          </p>
-        </li>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">Backend</h3>
-          <p className="bio-skill__content">
-            MongoDB (Mongoose), Node.js, Express.js, RESTful Routes.
-          </p>
-        </li>
-        <li className="bio-skill" data-test-bio-skill>
-          <h3 className="bio-skill__header">Others</h3>
-          <p className="bio-skill__content">
-            Python3, Jinja Templating, Flask, C.
-          </p>
-        </li>
+        {allSkills}
       </ul>
     </section>
   );
--- src/components/helpers/skills-info.js
+++ src/components/helpers/skills-info.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-param-reassign */
-
 const skills = [
   {
     section: 'frontend',

e125546d4ae185bc8825f25f12b33bf92784d93f
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-07-02 15:58:06 -0700

    [refactor] add semantic link to identify active page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-const Footer = () => (
+const Footer = ({ location }) => (
   <footer className="portfolio__footer" data-test-portfolio-footer-wrapper>
-    <small className="footer__additional-info" data-test-footer-additional-info>
-      Click on the image for project details
-    </small>
+    {location.pathname === '/projects' && (
+      <small
+        className="footer__additional-info"
+        data-test-footer-additional-info
+      >
+        Click on the image for project details
+      </small>
+    )}
     <a
-      className="footer__contact-link"
+      className="footer__contact-link email"
       data-test-portfolio-contact-link
       href="mailto:julie.hutchinson001@outlook.com"
     >
       EMAIL
     </a>
     <a
-      className="footer__contact-link"
+      className="footer__contact-link github"
       data-test-portfolio-contact-link
       target="_blank"
       rel="noopener noreferrer"
@@ -22,7 +29,7 @@ const Footer = () => (
       GITHUB
     </a>
     <a
-      className="footer__contact-link"
+      className="footer__contact-link linkedin"
       data-test-portfolio-contact-link
       target="_blank"
       rel="noopener noreferrer"
@@ -33,4 +40,8 @@ const Footer = () => (
   </footer>
 );
 
-export default Footer;
+Footer.propTypes = {
+  location: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default withRouter(Footer);

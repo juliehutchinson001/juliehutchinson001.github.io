2baa060566cd9db8edd0d9ff57702a6be99a5337
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-07-02 20:02:44 -0700

    [feat] populate content of individual blog post including routing

--- src/components/blogs/blog-dashboard.js
+++ src/components/blogs/blog-dashboard.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import slugify from '../helpers/slugify';
+import blogs from './constants';
 
-const BlogDashboard = () => (
-  <div>
-    <p>this is the blog dashboard</p>
-    <Link to="/blogs/id1">First Post</Link>
-    <Link to="/blogs/id2">Second Post</Link>
-  </div>
-);
+const BlogDashboard = ({ match }) => {
+  const allBlogs = blogs.map(({ blogName, blogAbstract }) => (
+    <li
+      key={blogName}
+      className="individual-blog__section--wrapper"
+      data-test-individual-blog-section-wrapper
+    >
+      <Link
+        className="individual-blog__header--link"
+        to={`${match.url}/${slugify(blogName)}`}
+        data-test-blog-header-link
+      >
+        <h1
+          className="individual-blog__header"
+          data-test-blog-header-project-header
+        >
+          {blogName}
+        </h1>
+      </Link>
+      <p className="individual-blog__abstract" data-test-blog-abstract>
+        {blogAbstract}
+      </p>
+    </li>
+  ));
 
-export default BlogDashboard;
+  return (
+    <main className="main-blog__section" data-test-main-blog-section>
+      <h1
+        className="main-blog__section--header"
+        data-test-main-blog-section-header
+      >
+        this is the blog dashboard title
+      </h1>
+      <h2
+        className="main-blog__section--subheader"
+        data-test-main-blog-section-subheader
+      >
+        this is the blog dashboard subtitle
+      </h2>
+      <ul className="main-blog__section--list" data-test-main-blog-section-list>
+        {allBlogs}
+      </ul>
+    </main>
+  );
+};
+
+BlogDashboard.propTypes = {
+  match: PropTypes.objectOf([PropTypes.string]).isRequired,
+};
+
+export default withRouter(BlogDashboard);
--- src/components/blogs/blog.js
+++ src/components/blogs/blog.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import blog from './constants';
+import slugify from '../helpers/slugify';
 
-const Blog = props => {
-  const { match } = props;
+const Blog = ({ match }) => {
+  const { id } = match.params;
+  const blogToDisplay = blog.find(
+    eachBlog => slugify(eachBlog.blogName) === id
+  );
   return (
-    <div>
-      <p>this is the first blog with the id {match.params.id}</p>
-    </div>
+    <main className="main__blog-content--wrapper" data-test-main-blog-wrapper>
+      <h1 className="blog__header" data-test-blog-header>
+        {blogToDisplay.blogName}
+      </h1>
+      <p className="blog__content" data-test-blog-content>
+        {blogToDisplay.description}
+      </p>
+    </main>
   );
 };
 
 Blog.propTypes = {
-  match: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.element,
-    PropTypes.string,
-  ]).isRequired,
+  match: PropTypes.objectOf(PropTypes.bool).isRequired,
 };
 
 export default Blog;
